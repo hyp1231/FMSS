3dc6f9398d443e1da55efdb06956587744e1b85d
prime51 <26063384+prime51@users.noreply.github.com>
2018-05-14 18:52:45 +0800

    Update disk.cpp
    Not found diskfile -> Diskfile not found
    单纯为了增加点存在感 hhh

--- disk.cpp
+++ disk.cpp
@@ -19,7 +19,7 @@ Disk::~Disk() {
 bool Disk::Getblk(char buf[], int blk_num) {
 	ifstream ifile(disk_file);
 	if(!ifile) {	// cannot open file
-		cout << "Not found diskfile... (´Д` )" << endl;
+		cout << "Diskfile not found... (´Д` )" << endl;
 		return false;
 	}
 	ifile.seekg(ifile.beg + BLKsize * blk_num);
@@ -33,7 +33,7 @@ bool Disk::Getblk(char buf[], int blk_num) {
 bool Disk::Putblk(char buf[], int blk_num) {
 	ofstream ofile(disk_file, ios::out | ios::in);
 	if(!ofile) {	// cannot open file
-		cout << "Not found diskfile... (´Д` )" << endl;
+		cout << "Diskfile not found... (´Д` )" << endl;
 		return false;
 	}
 	ofile.seekp(ofile.beg + BLKsize * blk_num);
