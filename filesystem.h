6b721b2b9dc643a38b30fcad8a42d4fa50ce3ccd
hyp1231 <houyupeng@ruc.edu.cn>
2018-05-18 18:30:13 +0800

    fix some funcs' name and add a func

--- filesystem.h
+++ filesystem.h
@@ -10,18 +10,19 @@ private:
 	const int BLKsize;
 
 	Disk D;					// disk
-	int root_dir_inode;		// inode of root dir
-	int cur_dir_inode;		// inode of cur dir
+	int root_dir_inodeNum;		// inodeNum of root dir
+	int cur_dir_inodeNum;		// inodeNum of cur dir
 	char inode_bitmap[10];
 	char data_block_bitmap[80];
 
     int Get_actual_dataBLKnumber(int n);
-    int Get_dir_inode(vector<string>& path);
+    int Get_dir_inodeNum_from_path(vector<string>& path);
+    int Get_file_inodeNum_from_dir(int dir_inodeNum, const string& filename);
 public:
 	FileSystem();
 	~FileSystem();
 	void Help();
     bool CreateFile(const string &filepath);
     //bool DeleteFile(const string &filepath);
-    int Get_cur_dir_inode();
+    int Get_cur_dir_inodeNum();
 };
